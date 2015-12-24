d158fbdd93ded2d90cc6e53d4118e3cfcdfc88d7
Kyle Fenstermacher <colonelkyle@gmail.com>
2015-12-24 12:14:41 -0500

    initial commit of comments in js

---
+++ space.js
@@ -0,0 +1,10 @@
+//game config
+//game loop
+//create ship
+//create invaders
+//create missiles
+//create 2d array of invaders
+//check collisions between invaders, missiles, and ship
+//draw ship
+//draw invaders
+//draw missiles
