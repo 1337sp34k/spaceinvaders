6811ef4ee7b0ca26a6c52fedc71f2fcd6d55b13e
Kyle Fenstermacher <colonelkyle@gmail.com>
2015-12-26 17:22:39 -0500

    reorganized code

--- space.js
+++ space.js
@@ -9,7 +9,15 @@ this.missArr = [];
 this.missHeight = 10;
 this.missWidth = 2;
 this.invaderGap = 5;
+this.invaderHeight = 10;
+this.invaderWidth = 25;
+this.invaderDx = 5;
+this.invaderDy = this.invaderGap + this.invaderHeight;
 //game loop
+
+/***************************************************
+* Ship code --
+***************************************************/
 //create ship
 function Ship(x){
   this.x = x;
@@ -17,48 +25,7 @@ function Ship(x){
   this.width = 25
 }
 this.ship = new Ship(this.maxX/2)
-//create invaders
-//create missiles
-function Missile(x, y){
-  this.x = x;
-  this.y = y;
-}
-function invade(x, y){
-  this.x = x;
-  this.y = y;
-  this.height = 10;
-  this.width = 25;
-}
 
-function invadeArrs(){
-  var rankFile = [];
-
-
-}
-
-//create 2d array of invaders
-//check collisions between invaders, missiles, and ship
-//key handler
-function checkKey(event){
-  event = event || window.event;
-  if (event.keyCode == '37'){
-    //move ship left
-    moveShip(-10);
-    drawShip();
-  }
-  else if (event.keyCode == '39') {
-    //move ship right
-    moveShip(10);
-    drawShip();
-  }
-  else if (event.keyCode == '32') {
-      //fire missile
-      var missile = new Missile(this.ship.x + this.ship.width/2 - this.missWidth/2,
-              this.maxY - this.ship.height - this.missHeight);
-      this.missArr.push(missile);
-      drawMissile(missile);
-  }
-}
 //move ship
 function moveShip(dist){
   this.ship.x += dist;
@@ -83,6 +50,19 @@ function drawShip(){
 }
 drawShip();
 
+//create invaders
+//create missiles
+
+/***************************************************
+* Missile code --
+***************************************************/
+function Missile(x, y){
+  this.x = x;
+  this.y = y;
+}
+
+
+
 //draw misiles
 function drawMissile(missile){
   var canvas = document.getElementsByTagName('canvas')[0];
@@ -90,11 +70,49 @@ function drawMissile(missile){
   context.fillStyle = 'red';
   context.fillRect(missile.x, missile.y, this.missWidth, this.missHeight);
 }
+/***************************************************
+* Invader code --
+***************************************************/
+function Invader(r, c){
 
+  this.alive = true;
+  this.row = r;
+  this.col = c;
+}
+
+function invadeArrs(){
+  var rankFile = [];
+
+
+}
 //draw invaders
-function drawInvade(){
+function drawInvader(x, y){
   var canvas = document.getElementsByTagName('canvas')[0];
   var context = canvas.getContext('2d');
-  context.fillRect(x, this.maxY-this.missHeight, this.missWidth, this.missHeight);
   context.fillStyle = 'green';
+  context.fillRect(x, y, this.invaderWidth, this.invaderHeight);
+}
+
+//create 2d array of invaders
+//check collisions between invaders, missiles, and ship
+//key handler
+function checkKey(event){
+  event = event || window.event;
+  if (event.keyCode == '37'){
+    //move ship left
+    moveShip(-10);
+    drawShip();
+  }
+  else if (event.keyCode == '39') {
+    //move ship right
+    moveShip(10);
+    drawShip();
+  }
+  else if (event.keyCode == '32') {
+      //fire missile
+      var missile = new Missile(this.ship.x + this.ship.width/2 - this.missWidth/2,
+              this.maxY - this.ship.height - this.missHeight);
+      this.missArr.push(missile);
+      drawMissile(missile);
+  }
 }
